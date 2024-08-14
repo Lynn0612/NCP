0e577f2f5dabc6daf1aa4190eeb1c77be9b81b3c
Lynn <lynn.cirous@gmail.com>
2024-08-14 15:49:35 +0800

    feature:FAQ新增陣列方式

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -6,6 +6,28 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 const Faq = () => {
+    const data = [
+        {
+            question: 'How does the card work?',
+            answer: 'This is the answer to "How does the card work?"'
+        },
+        {
+            question: 'Where is the card available?',
+            answer: 'This is the answer to "Where is the card available?"'
+        },
+        {
+            question: 'What is the difference between plastic and virtual cards?',
+            answer: 'This is the answer to "What is the difference between plastic and virtual cards?"'
+        },
+        {
+            question: 'How many days will it take to issue my card?',
+            answer: 'This is the answer to "How many days will it take to issue my card?"'
+        },
+        {
+            question: 'Are there any fees?',
+            answer: 'This is the answer to "Are there any fees?"'
+        }
+    ];
     return (
         <div className="faq">
             <div>
@@ -15,8 +37,33 @@ const Faq = () => {
                 </span>
             </div>
             <hr class="hr my-2"></hr>
+            <ul className="list-group list-group-flush">
+                {data.map((item, index) => (
+                    <li className="list-group-item py-4" key={index}>
+                        <div
+                            className="d-flex justify-content-between align-items-center"
+                            data-bs-toggle="collapse"
+                            href={`#collapse${index}`}
+                            role="button"
+                            aria-expanded="false"
+                            aria-controls="collapseExample"
+                        >
+                            {item.question}
+                            <div className="circle">
+                                <i className="bi bi-plus"></i>
+                            </div>
+                        </div>
+                        <div className="collapse mt-2" id={`collapse${index}`}>
+                            <div className="p-3">
+                                {item.answer}
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+            <hr className="hr-last m-0"></hr>
 
-            <ul class="list-group list-group-flush">
+            {/* <ul class="list-group list-group-flush">
                 <li class="list-group-item py-4">
                     <div
                         class="d-flex justify-content-between align-items-center"
@@ -122,7 +169,7 @@ const Faq = () => {
                     </div>
                 </li>
                 <li class="list-group-item py-4"></li>
-            </ul>
+            </ul> */}
         </div>
     )
 }
