fcffdbd0b22164a54bb738ab9940378562d1cb3d
Jack <jack.cirous@gmail.com>
2024-08-16 17:00:03 +0800

    feature: 新增 News API 來源

--- src/resources/api.js
+++ src/resources/api.js
@@ -1,6 +1,9 @@
 export const getFaqs = () => {
     return getData(`v1/faqs`);
 };
+export const getNews = () => {
+    return getData(`v1/news`);
+};
 
 const getData = (uri, data) => {
     let url = `${process.env.REACT_APP_API_URL}${uri}`;
