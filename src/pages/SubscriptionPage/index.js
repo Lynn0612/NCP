36ef147d5438fd90df51b533c926d49795282bbc
Lynn <lynn.cirous@gmail.com>
2024-09-20 15:15:08 +0800

    feature: subscription banner切版 https://trello.com/c/VarIv1eC

---
+++ src/pages/SubscriptionPage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+import SubscriptionBanner from './banner/SubscriptionBanner';
+
+
+
+const SubscriptionPage = () => {
+
+    return (
+        <>
+            <SubscriptionBanner />
+        </>
+    );
+};
+export default SubscriptionPage;
